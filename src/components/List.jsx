7d47c271a3b7ea32606fd45bcd4c6bf9ce33aec2
Somwanshi Swaraj <somwanshiswaraj4@gmail.com>
2025-11-22 20:13:39 +0530

    Handle Delete

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,18 +1,30 @@
+import { useContext } from "react";
+import { TaskContext } from "../context/Context";
 
-const List = () => {  
+const List = ({elem}) => {  
 
+  let {allTasks, setAllTasks} = useContext(TaskContext)
+
+  const handleDelete = ()=> {
+
+    let filterArr = allTasks.filter((val) => val.id !== elem.id);
+    localStorage.setItem("tasks", JSON.stringify(filterArr));
+    setAllTasks(filterArr);
+
+    console.log("deleted:", elem.task );
+  }
 
 
   return (
     <div className="bg-white border border-gray-300 rounded-md p-2 flex gap-6 items-center justify-between">
       <p className="w-full p-2">
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Modi.
+        {elem.task}
       </p>
       <div className="controls flex gap-2 *:rounded-md">
         <button  className="py-1.5 px-2.5 bg-green-300 text-white font-semibold cursor-pointer active:scale-95 hover:bg-green-400">
           Update
         </button>
-        <button  className="py-1.5 px-2.5 bg-red-500 text-white font-semibold cursor-pointer active:scale-95 hover:bg-red-600">
+        <button onClick={handleDelete}  className="py-1.5 px-2.5 bg-red-500 text-white font-semibold cursor-pointer active:scale-95 hover:bg-red-600">
           Delete
         </button>
       </div>
