6d34b7b948f1f6029193a989745084ed848a7ad3
Somwanshi Swaraj <somwanshiswaraj4@gmail.com>
2025-11-22 20:05:14 +0530

    mapping list comp in taskcontainer and pass elem as prop for list to know which task to display

--- src/components/Taskcontainer.jsx
+++ src/components/Taskcontainer.jsx
@@ -1,13 +1,19 @@
 
+import { useContext } from 'react'
 import List from './List'
+import { TaskContext } from '../context/Context'
 
 const Taskcontainer = () => {
 
+  let {allTasks} = useContext(TaskContext);
+
   return (
     <div className='bg-gray-100 h-full border-2 border-dashed rounded-xl flex flex-col gap-1 p-2'>
-        {/* {allTasks?.map((elem)=> {
-            return <List key={elem.id} elem={elem}/>
-        })} */}
+        {
+          allTasks?.map((elem) => {
+            return <List key={elem.id} elem={elem} />
+          })
+        }
     </div>
   )
 }
