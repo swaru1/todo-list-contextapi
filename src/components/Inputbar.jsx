d6c980c4b5b3b85f37981ce63a347397ccb96b6f
Somwanshi Swaraj <somwanshiswaraj4@gmail.com>
2025-11-22 19:15:41 +0530

    two way binding of input

--- src/components/Inputbar.jsx
+++ src/components/Inputbar.jsx
@@ -1,14 +1,25 @@
+import { useContext, useState } from "react";
+import { TaskContext } from "../context/Context";
 
 
 const Inputbar = () => {
-
-
+  //using the context and destrc the value object
+  let {allTasks, setAllTasks} = useContext(TaskContext);
   
+  const [taskInp, setTaskInp] = useState("");
+  
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("form submit");
+  }
 
   return (
     <div className="flex justify-center">
-      <form className="flex gap-4 w-full justify-center">
+      <form onSubmit={handleSubmit} className="flex gap-4 w-full justify-center">
         <input
+          onChange={(e)=> setTaskInp(e.target.value)}
+          value={taskInp}
           type="text"
           className="border-2 focus:border-blue-400 outline-none py-1.5 px-2.5 rounded-md w-[50%]"
         />
